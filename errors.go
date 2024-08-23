a8b50c43302fcf21ff714fa588a2b04707a2048a
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2024-08-23 19:33:10 +0530

    Decouple insert

--- errors.go
+++ errors.go
@@ -6,6 +6,14 @@ type cantMakeDirError struct {
 	dir string
 }
 
+type insertError struct {
+	errorMessage string
+}
+
 func (err *cantMakeDirError) Error() string {
 	return fmt.Sprintf("Can't make directory %v\n", err.dir)
+}
+
+func (err *insertError) Error() string {
+	return fmt.Sprintf("There was an error inserting %v\n", err.errorMessage)
 }
--- insert.go
+++ insert.go
@@ -7,20 +7,47 @@ import (
 	"strings"
 )
 
+var insertQuery string = `
+INSERT INTO messages (message) VALUES(?)
+`
+
+var endCommand string = "/done"
+
 func (app *Application) insertMessage() error {
-	fmt.Printf("\x1b[1m")
-	messageReader := bufio.NewReader(os.Stdin)
-	message, err := messageReader.ReadString('\n')
-	message = strings.TrimSpace(message)
+	message, err := insertInput()
 	if err != nil {
-		return err
+		return &insertError{err.Error()}
 	}
-	insertQuery := `
-	INSERT INTO messages (message) VALUES(?)
-	`
-	_, err = app.db.Exec(insertQuery, message)
+	err = app.insertDB(message)
+	if err != nil {
+		return &insertError{err.Error()}
+	}
+	return nil
+}
+
+func (app *Application) insertDB(message string) error {
+	_, err := app.db.Exec(insertQuery, message)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func insertInput() (string, error) {
+	fmt.Printf("Type away! When you are done, type %q on a new line: \n", endCommand)
+	fmt.Printf("\x1b[1m")
+	reader := bufio.NewReader(os.Stdin)
+	input := make([]string, 0)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return "", err
+		}
+		line = strings.TrimSpace(line)
+		if line == "/done" {
+			break
+		}
+		input = append(input, line)
+	}
+	return strings.Join(input, "\n"), nil
 }
